64e1275cf75633f526a55d65ae6698fca9edeb17
ualase444 <ase444@inlumine.ual.es>
2025-10-22 17:58:39 +0200

    Initial Commit

---
+++ habitapp4/habitapp4App.swift
@@ -0,0 +1,17 @@
+//
+//  habitapp4App.swift
+//  habitapp4
+//
+//  Created by Aula03 on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct habitapp4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
